2f26c6a2515fb0b46099f66e90ab39efc203567a
ambiwalencja <helena.a.sokolowska@gmail.com>
2021-06-20 20:59:39 +0200

    added class Weather and checking if date is correct

--- weather.py
+++ weather.py
@@ -11,51 +11,86 @@ from requests.auth import HTTPBasicAuth
 # tutaj url: http://api.weatherapi.com/v1/forecast.json
 
 
-# class GetData:
-#     def __init__(self, url):
-#         self.url = url
-#         self.data = self.get_data()  # data will be in json format
-#
-#     def get_data(self, key):
-#         resp = requests.get(self.url)
-#         pprint.pprint(resp.json())
-#         return resp.json()
-#
-#
-# class Weather:
-#     def __init__(self, date):
-#         self.town = ''
-#         self.date = date
-#         self.time = ''
+class Weather:
+    def __init__(self, url, town, days):
+        self.url = url
+        self.town = town
+        self.date = datetime.datetime.strptime(sys.argv[2], "%Y-%m-%d").date()
+        self.forecast_length = days
+        self.data = self.get_data()  # data will be in json format
+
+    def check_date(self):
+        difference = self.date - datetime.datetime.today().date()
+        if difference.days > 10:
+            return False
+        if difference.days < 0:
+            return False
+        return True
+
+    def get_data(self):
+        if not self.check_date():
+            return False
+        api_key = sys.argv[1]
+        request_params = {'key': api_key, 'q': self.town, 'days': self.forecast_length, 'dt': self.date}
+        response = requests.get(self.url, params=request_params)
+        # pprint.pprint(response.json())
+        return response.json()
+
+    def get_rain_info(self):
+        rain = self.get_data()['forecast']['forecastday'][0]['day']['daily_will_it_rain']
+        return rain
+
+    def get_snow_info(self):
+        snow = self.get_data()['forecast']['forecastday'][0]['day']['daily_will_it_snow']
+        return snow
+
+    def write_to_file(self):
+        with open("forecast.txt", "a") as file:
+            file.write(f'{self.date},{self.get_rain_info()},{self.get_snow_info()}\n')
 
 
-# my_data = GetData("http://api.weatherapi.com/v1/forecast.json")
+# print("Hello. This program will tell you if it will rain or snow in Warsaw"
+#       "on given day up to 10 days from now. To get information about chosen day type"
+#       "your secret code (API key) and the date in format YYYY-MM-DD. Enjoy!")
+
+my_weather = Weather("http://api.weatherapi.com/v1/forecast.json", "Warsaw", 10)
+if not my_weather.data:
+    print("Couldn't load data. Please select date between today and 10 days forward.")
+else:
+    if my_weather.get_rain_info():
+        print("It will rain.")
+    if my_weather.get_snow_info():
+        print("It will snow.")
+    if not my_weather.get_rain_info() and not my_weather.get_snow_info():
+        print("It will be clear.")
+    my_weather.write_to_file()
+
 # print(my_data.data)
 
 
-api_key = sys.argv[1]
-# town = sys.argv[2]
-town = 'Warsaw'
-date = datetime.datetime.strptime(sys.argv[2], "%Y-%m-%d").date()
+# api_key = sys.argv[1]
+# # town = sys.argv[2]
+# town = 'Warsaw'
+# date = datetime.datetime.strptime(sys.argv[2], "%Y-%m-%d").date()
+#
+# request_params = {'key': api_key,
+#                   'q': town,
+#                   'days': 10,
+#                   'dt': date}
+# resp = requests.get("http://api.weatherapi.com/v1/forecast.json", params=request_params)
 
-request_params = {'key': api_key,
-                  'q': town,
-                  'days': 10,
-                  'dt': date}
-resp = requests.get("http://api.weatherapi.com/v1/forecast.json", params=request_params)
 
-
-will_it_rain = resp.json()['forecast']['forecastday'][0]['day']['daily_will_it_rain']
-will_it_snow = resp.json()['forecast']['forecastday'][0]['day']['daily_will_it_snow']
-# will_it = 0
-if not will_it_rain and not will_it_snow:
-    # will_it = 0
-    print('It will neither rain nor snow.')
-else:
-    print("It will rain or snow.")
-
-with open("forecast.txt", "a") as file:
-    file.write(f'{date},{will_it_rain},{will_it_snow}\n')
+# will_it_rain = resp.json()['forecast']['forecastday'][0]['day']['daily_will_it_rain']
+# will_it_snow = resp.json()['forecast']['forecastday'][0]['day']['daily_will_it_snow']
+# # will_it = 0
+# if not will_it_rain and not will_it_snow:
+#     # will_it = 0
+#     print('It will neither rain nor snow.')
+# else:
+#     print("It will rain or snow.")
+#
+# with open("forecast.txt", "a") as file:
+#     file.write(f'{date},{will_it_rain},{will_it_snow}\n')
 
 
 
