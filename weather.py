dbac6158abc9b04f9aa731962e53cc00410f6867
ambiwalencja <helena.a.sokolowska@gmail.com>
2021-06-20 21:53:48 +0200

    added reading data from file but now my code is chaotic

--- weather.py
+++ weather.py
@@ -2,7 +2,6 @@ import sys
 import requests  # https://docs.python-requests.org/en/master/
 import pprint  # pretty print https://docs.python.org/3/library/pprint.html
 import datetime
-from requests.auth import HTTPBasicAuth
 
 # API Key: 57836734f5964a9a970213219211406
 # https://stackoverflow.com/questions/53075939/calling-rest-api-with-an-api-key-using-the-requests-package-in-python
@@ -17,7 +16,10 @@ class Weather:
         self.town = town
         self.date = datetime.datetime.strptime(sys.argv[2], "%Y-%m-%d").date()
         self.forecast_length = days
-        self.data = self.get_data()  # data will be in json format
+        # self.data = self.get_data()  # data will be in json format
+        self.rain = 0
+        self.snow = 0
+        self.get_data()
 
     def check_date(self):
         difference = self.date - datetime.datetime.today().date()
@@ -27,26 +29,43 @@ class Weather:
             return False
         return True
 
+    def read_from_file(self):
+        with open("forecast.txt", "r") as file:
+            for line in file.readlines():
+                date_in_line = datetime.datetime.strptime(line.split(",")[0], "%Y-%m-%d").date()
+                if date_in_line == self.date:
+                    self.rain = line.split(",")[1]
+                    self.snow = line.split(",")[2].split("\n")[0]
+                    return True
+            return False
+
     def get_data(self):
         if not self.check_date():
             return False
+        if self.read_from_file():
+            return True
         api_key = sys.argv[1]
         request_params = {'key': api_key, 'q': self.town, 'days': self.forecast_length, 'dt': self.date}
         response = requests.get(self.url, params=request_params)
+        self.rain = response.json()['forecast']['forecastday'][0]['day']['daily_will_it_rain']
+        self.snow = response.json()['forecast']['forecastday'][0]['day']['daily_will_it_snow']
+        # print(f'debug: rain {self.rain}, snow {self.snow}.')
         # pprint.pprint(response.json())
-        return response.json()
+        # return response.json()
+        self.write_to_file()
+        return True
 
-    def get_rain_info(self):
-        rain = self.get_data()['forecast']['forecastday'][0]['day']['daily_will_it_rain']
-        return rain
-
-    def get_snow_info(self):
-        snow = self.get_data()['forecast']['forecastday'][0]['day']['daily_will_it_snow']
-        return snow
+    # def get_rain_info(self):
+    #     self.rain = self.get_data()['forecast']['forecastday'][0]['day']['daily_will_it_rain']
+    #     return self.rain
+    #
+    # def get_snow_info(self):
+    #     self.snow = self.get_data()['forecast']['forecastday'][0]['day']['daily_will_it_snow']
+    #     return self.rain
 
     def write_to_file(self):
         with open("forecast.txt", "a") as file:
-            file.write(f'{self.date},{self.get_rain_info()},{self.get_snow_info()}\n')
+            file.write(f'{self.date},{self.rain},{self.snow}\n')
 
 
 # print("Hello. This program will tell you if it will rain or snow in Warsaw"
@@ -54,16 +73,19 @@ class Weather:
 #       "your secret code (API key) and the date in format YYYY-MM-DD. Enjoy!")
 
 my_weather = Weather("http://api.weatherapi.com/v1/forecast.json", "Warsaw", 10)
-if not my_weather.data:
+if not my_weather.get_data():
     print("Couldn't load data. Please select date between today and 10 days forward.")
 else:
-    if my_weather.get_rain_info():
+    # print(f'debug 4: rain {my_weather.rain}, snow {my_weather.snow}.')
+    if int(my_weather.rain) == 1:
+        # print(f'debug 2: rain {my_weather.rain}, snow {my_weather.snow}.')
         print("It will rain.")
-    if my_weather.get_snow_info():
+    if int(my_weather.snow) == 1:
+        # print(f'debug 3: rain {my_weather.rain}, snow {my_weather.snow}.')
         print("It will snow.")
-    if not my_weather.get_rain_info() and not my_weather.get_snow_info():
+    if int(my_weather.rain) == 0 and int(my_weather.snow) == 0:
         print("It will be clear.")
-    my_weather.write_to_file()
+    # my_weather.write_to_file()
 
 # print(my_data.data)
 
